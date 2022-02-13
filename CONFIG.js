15445f7aee7799fef58a4f9cb453b19bf31e0d73
cyberlove1 <88518104+cyberlove1@users.noreply.github.com>
2022-02-13 17:36:38 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -6,5 +6,5 @@ const CONFIGDATA = {
   buttonNo: "Không! Còn lâu nhé.",
   titleModar: "Okela...",
   descriptionModar:
-    'Vì lý do địa lý nên khó mà gửi đồ đc </br> Cho nợ đi nào gặp toi trả sau :)) </br> Từ mai c học r nên khó có thể rủ game đc. </br>  Chúc b học tốt.
+    'Vì lý do địa lý nên khó mà gửi đồ đc </br> Cho nợ đi nào gặp toi trả sau :)) </br> Từ mai b học r nên khó có thể rủ game đc. </br>  Chúc b học tốt.
 };
